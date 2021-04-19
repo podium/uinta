146c42e4ea62faa73f87e6db253da1218a1f5a09
emmanuel.pinault <emmanuel.pinault@podium.com>
2021-04-19 10:50:04 -0700

    fix: Duration was incorrect and is now in milliseconds

--- test/uinta/plug_test.exs
+++ test/uinta/plug_test.exs
@@ -136,7 +136,7 @@ defmodule Uinta.PlugTest do
       end)
 
     assert message =~
-             ~r"{\"duration_ms\":[0-9]+,\"method\":\"GET\",\"path\":\"/\",\"status\":\"200\",\"timing\":\"[0-9]+[µm]s\"}"u
+             ~r"{\"duration_ms\":[0-9]+\.?[0-9]+,\"method\":\"GET\",\"path\":\"/\",\"status\":\"200\",\"timing\":\"[0-9]+[µm]s\"}"u
   end
 
   test "logs graphql json to console" do
@@ -149,7 +149,7 @@ defmodule Uinta.PlugTest do
       end)
 
     assert message =~
-             ~r"{\"duration_ms\":[0-9]+,\"method\":\"QUERY\",\"path\":\"getUser\",\"status\":\"200\",\"timing\":\"[0-9]+[µm]s\"}"u
+             ~r"{\"duration_ms\":[0-9]+\.?[0-9]+,\"method\":\"QUERY\",\"path\":\"getUser\",\"status\":\"200\",\"timing\":\"[0-9]+[µm]s\"}"u
   end
 
   test "logs paths with double slashes and trailing slash" do
